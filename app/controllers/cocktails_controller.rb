2c0bd80b72595dcd1d18f73500bfc0ab478d84c4
Matthew Frazier <frazier3388@yahoo.com>
2018-08-10 17:34:03 +0900

    Begain the Create Function

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,19 @@
 class CocktailsController < ApplicationController
-  before_action :find_cocktail_id, only: [:show]
+  before_action :find_cocktail_id, only: [:show, :create]
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    # @cocktail.save
+    # redirect_to cocktail_path(@cocktail.id)
   end
 
   private
